5884e625c03f4d1ace8b68ac1bde16f1faa16bfa
george14051 <87715790+george14051@users.noreply.github.com>
2021-07-26 21:46:14 +0300

    Add files via upload

---
+++ mta_blockchain/market/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '2951c323a8753db56414d2b2'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+from market import routes
+from market import models
+
